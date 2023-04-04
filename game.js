b4ffe2ebf0b35764bebacfdd7d05310b8e308764
Gustavo Escocard <gustavo.escocard@gmail.com>
2023-04-03 21:35:16 -0300

    Create end game page and display the final score

--- game.js
+++ game.js
@@ -49,8 +49,8 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
         return window.location.assign("./end.html");
     }
 
